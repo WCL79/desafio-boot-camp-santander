94232cdb38b55dc4e29b969ccfd31374aa20867a
weslley lima <weslleyclima@yahoo.com.br>
2021-06-27 19:08:37 -0300

    Primiero commit mas sem fluxo cuja troca de branch

---
+++ src/main/java/br/com/dio/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package br.com.dio.repository;
+
+import br.com.dio.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
---
+++ src/test/java/br/com/dio/PontoAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
